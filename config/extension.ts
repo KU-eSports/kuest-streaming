6058c655024422477b0868cadc3b3e06c9aad73b
secchanu <39504565+secchanu@users.noreply.github.com>
2022-05-25 09:31:13 +0900

    resolve node_modules

--- config/extension.ts
+++ config/extension.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   build: {
     target: "esnext",
-    polyfillDynamicImport: false,
     lib: {
       entry: "./src/extension/index.ts",
       fileName: () => "index.js",
       formats: ["cjs"]
+    },
+    rollupOptions: {
+      external: id => !(path.isAbsolute(id) || id.startsWith("."))
     },
     outDir: "extension",
     emptyOutDir: true
