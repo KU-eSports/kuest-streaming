fe646b960262dc0da579f6ccc8604566344e382b
secchanu <39504565+secchanu@users.noreply.github.com>
2022-05-25 09:21:35 +0900

    resolve node_modules

--- config/extension.ts
+++ config/extension.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   build: {
@@ -7,6 +8,12 @@ export default defineConfig({
       entry: "./src/extension/index.ts",
       fileName: () => "index.js",
       formats: ["cjs"]
+    },
+    rollupOptions: {
+      external: id => !(path.isAbsolute(id) || id.startsWith(".")),
+      output: {
+        
+      }
     },
     outDir: "extension",
     emptyOutDir: true
