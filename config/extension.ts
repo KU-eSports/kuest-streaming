7f76a5cc7171c11c8609c6a99b7c1c0ef3ec303d
secchanu <39504565+secchanu@users.noreply.github.com>
2022-05-25 09:32:35 +0900

    remove output

--- config/extension.ts
+++ config/extension.ts
@@ -10,10 +10,7 @@ export default defineConfig({
       formats: ["cjs"]
     },
     rollupOptions: {
-      external: id => !(path.isAbsolute(id) || id.startsWith(".")),
-      output: {
-        
-      }
+      external: id => !(path.isAbsolute(id) || id.startsWith("."))
     },
     outDir: "extension",
     emptyOutDir: true
