7f4e963e7b25fbac4a963ae5e6029235890c2ebb
secchanu <secchanu929@gmail.com>
2022-09-27 03:44:21 +0900

    delete valorant

--- src/nodecg/replicants.ts
+++ src/nodecg/replicants.ts
@@ -1,7 +1,6 @@
 import * as replicant from "./generated/index";
 
 type ReplicantMap = {
-  valorantMap: replicant.ValorantMap;
   waiting: replicant.Waiting;
   discordVoice: replicant.DiscordVoice;
 };
