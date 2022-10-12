e41ac58e448f0f7b8fc4bdeae2505c0b46031ad2
NYA0321 <kimito0321@stu.kanazawa-u.ac.jp>
2022-10-12 16:44:07 +0900

    info.tsx に仮のクラス名を追加 question mapnameなどはstyles.じゃない気がする

--- src/browser/graphics/talk/component/info.tsx
+++ src/browser/graphics/talk/component/info.tsx
@@ -3,7 +3,10 @@ import type { FunctionComponent } from "react";
 import styles from "../css/info.module.css";
 
 const Component: FunctionComponent = () => {
-  return <div></div>;
+  return <div className={styles.wrapper}>
+    <div className={styles.next}>Next Map</div>
+    <div className={styles.mapname}></div>  {/* TODO ここstyles.じゃない気がする */}
+  </div>;
 };
 
 export default Component;
