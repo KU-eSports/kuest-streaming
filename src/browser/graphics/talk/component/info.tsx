88e8c95e3aa5b9166132de6d40a340ad24bede63
NYA0321 <kimito0321@stu.kanazawa-u.ac.jp>
2022-10-14 12:55:43 +0900

    add wrapper class(info file's)

--- src/browser/graphics/talk/component/info.tsx
+++ src/browser/graphics/talk/component/info.tsx
@@ -3,7 +3,7 @@ import type { FunctionComponent } from "react";
 import styles from "../css/info.module.css";
 
 const Component: FunctionComponent = () => {
-  return <div className={styles.wrapper}>
+ return <div className={styles.wrapper}>
     <div className={styles.next}>Next Map</div>
     <div className={styles.mapname}>ヤガラ市場</div>
   </div>;
