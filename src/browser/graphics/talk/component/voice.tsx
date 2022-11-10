981cee671cc11c98bf49628e1819a26ffcc6af88
secchanu <secchanu929@gmail.com>
2022-11-11 00:57:08 +0900

    add icon

--- src/browser/graphics/talk/component/voice.tsx
+++ src/browser/graphics/talk/component/voice.tsx
@@ -2,11 +2,19 @@ import type { FunctionComponent } from "react";
 
 import styles from "../css/voice.module.css";
 
+import toomo from "../image/Toomo.png";
+import naka from "../image/N4K4.png";
+const talker: { [key: string]: string | undefined } = {
+  "346592327701102594": toomo,
+  "402750037739831298": naka,
+};
+
 type Props = {
   speaker: Speaker;
 };
 const Component: FunctionComponent<Props> = (props) => {
   const speaker = props.speaker;
+  const id = speaker.user.id;
   const avatar = speaker.user.avatar
     ? `https://cdn.discordapp.com/avatars/${speaker.user.id}/${speaker.user.avatar}.jpg`
     : `https://cdn.discordapp.com/embed/avatars/${
@@ -19,7 +27,7 @@ const Component: FunctionComponent<Props> = (props) => {
         className={`${styles.icon} ${
           speaker.speaking ? styles.speaking : styles.unspeaking
         }`}
-        src={avatar}
+        src={talker[id]}
       />
       <div className={styles.container}>
         <div className={styles.box}></div>
