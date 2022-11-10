bba75024312a6d7b9f78258e5c1e96dfee273d8e
secchanu <39504565+secchanu@users.noreply.github.com>
2022-11-10 21:29:47 +0900

    Waiting (#67)
    * talk init
    
    * add splatnet3
    
    * waiting layout の実装 (#63)
    
    Co-authored-by: NYAA <109290659+NYA0321@users.noreply.github.com>

---
+++ src/browser/graphics/waiting/component/background.tsx
@@ -0,0 +1,17 @@
+import type { FunctionComponent } from "react";
+
+import styles from "../css/background.module.css";
+import bg from "../image/bgpattern.svg";
+
+const Component: FunctionComponent = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div
+        style={{ backgroundImage: `url(${bg})` }}
+        className={styles.bgimg}
+      ></div>
+    </div>
+  );
+};
+
+export default Component;
--- src/browser/graphics/waiting/index.tsx
+++ src/browser/graphics/waiting/index.tsx
@@ -1,20 +1,29 @@
 import type { FunctionComponent } from "react";
 
-import Agent from "./component/agent";
+import Background from "./component/background";
+import Stickers from "./component/stickers";
 import Timer from "./component/timer";
+import Splats from "./component/splats";
 import Telop from "./component/telop";
 
 import "modern-normalize";
+import "../../common/css/splatnet.css";
 import styles from "./css/style.module.css";
 
 const Component: FunctionComponent = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.agent}>
-        <Agent />
+    <div className={styles.wrapper}>
+      <div className={styles.background}>
+        <Background />
+      </div>
+      <div className={styles.stickers}>
+        <Stickers />
       </div>
       <div className={styles.timer}>
         <Timer />
+      </div>
+      <div className={styles.splats}>
+        <Splats />
       </div>
       <div className={styles.telop}>
         <Telop />
